bb84d1085f4d94bc93432670f14c51e7256fa57a
Lucas Menendez <hi@lucasmenendez.me>
2024-09-26 21:26:27 +0200

    create a crate

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod utils;
+use groth16_verifier::verify_from_json;
 
 fn main() {
     let vk_json = std::fs::read_to_string("./data/verification_key.json").unwrap();
     let proof_json = std::fs::read_to_string("./data/proof.json").unwrap();
     let public_inputs_json = std::fs::read_to_string("./data/public_inputs.json").unwrap();
 
-    let is_valid = utils::verify_from_json(vk_json, proof_json, public_inputs_json);
+    let is_valid = verify_from_json(vk_json, proof_json, public_inputs_json);
     println!("Is valid: {:?}", is_valid);
 }
